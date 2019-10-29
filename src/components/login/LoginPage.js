c2af6336ec79baddd82981bb9c727f09a807e292
Harrison Noble <harrisonnoble@yahoo.com>
2019-10-28 21:55:39 -0500

    added some component files, attempting to work on login system

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react';
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default LoginPage;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Politics</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Navbar;
