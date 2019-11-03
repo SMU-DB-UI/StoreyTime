d452534cf29a8a6d5091db7b88837a31846d61e2
Harrison Noble <harrisonnoble@yahoo.com>
2019-11-03 16:31:58 -0600

    nav bar and login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -7,46 +7,37 @@ class Login extends Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      error: '',
     };
   }
+
   render(){
     return (
-      <div className="limiter">
-        <div className="container-login100" style={{backgroundImage: 'url("images/bg-01.jpg")'}}>
-          <div className="wrap-login100">
-            <form className="login100-form validate-form">
-              <span className="login100-form-logo">
-                <i className="zmdi zmdi-landscape" />
-              </span>
-              <span className="login100-form-title p-b-34 p-t-27">
-                Log in
-              </span>
-              <div className="wrap-input100 validate-input" data-validate="Enter username">
-                <input className="input100" type="text" name="username" placeholder="Username" />
-                <span className="focus-input100" data-placeholder="" />
-              </div>
-              <div className="wrap-input100 validate-input" data-validate="Enter password">
-                <input className="input100" type="password" name="pass" placeholder="Password" />
-                <span className="focus-input100" data-placeholder="" />
-              </div>
-              <div className="contact100-form-checkbox">
-                <input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
-                <label className="label-checkbox100" htmlFor="ckb1">
-                  Remember me
-                </label>
-              </div>
-              <div className="container-login100-form-btn">
-                <button className="login100-form-btn">
-                  Login
-                </button>
-              </div>
-              <div className="text-center p-t-90">
-                <a className="txt1" href="#">
-                  Forgot Password?
-                </a>
-              </div>
-            </form>
+      <div className="container-login">
+        <div className="div-login">
+          <form className="login-form">
+            <span className="login-img">
+              <img src="https://placehold.it/150x150" alt="placehold"/>
+            </span>
+            <span className="login-title">Log In</span>
+            <br/>
+            <br/>
+            <div className="username-wrapper">
+              <span className="glyphicon glyphicon-user icon"></span> 
+              <input className="username" type="text" name="username" placeholder="Username"/>
+            </div>
+            <div className="password-wrapper">
+              <span className="glyphicon glyphicon-lock icon"></span> 
+              <input className="password" type="password" name="password" placeholder="Password"/>
+            </div>
+            <div className="button-wrapper">
+              <button type="button" className="btn btn-light">Login</button>
+            </div>
+          </form>
+          <div className="register-wrapper">
+            <p>Dont have an account? Register</p> 
+            <a href="#">Here</a>
           </div>
         </div>
       </div>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import './navbar.css';
 
 class Navbar extends Component {
     state = {  }
     render() { 
         return ( 
-            <div>
-                <h1>Politics</h1>
-            </div>
+            <header className="App-header">
+                <div className="head">
+                    <div className="left">
+                        <h1 className="name">Ballot Buddy</h1>
+                        <ul className="links">
+                            <li>Candidates</li>
+                            <li>Polls</li>
+                            <li>Voter Registration</li>
+                        </ul>
+                    </div>
+                <div className="right">
+                    <span>Log out</span>
+                    <span>Profile</span>
+                </div>
+                <div className="clear"></div></div>
+            </header>
          );
     }
 }
