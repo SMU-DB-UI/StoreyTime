95ccd9fc9d73f27b7961fa4c69bef3d606dc1c62
Harrison Noble <harrisonnoble@yahoo.com>
2019-11-13 17:23:42 -0600

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,6 @@ const mysql = require('mysql');
 var path = require('path');
 var crypto = require('crypto'); //hash
 
-//create the mysql connection object.  
-// var connection = mysql.createConnection({
-//   host: 'backend-db',
-//   port: '3306',
-//   user: 'user',
-//   password: 'password',
-//   database: 'ballotBuddy'
-// });
-
 //set up some configs for express. 
 const config = {
   name: 'backend',
@@ -24,41 +15,18 @@ const config = {
   host: '0.0.0.0',
 };
 
-const app = express();
 //create the express.js object
-
-//create objects for sessions
-
-// const flash = require('connect-flash');
-// const session = require('express-session');
-// app.use(session({
-//   secret: 'Text goes here',
-//   resave: true,
-//   saveUninitialized: true
-// }));
+const app = express();
 
 //create a logger object.  Using logger is preferable to simply writing to the console. 
 const logger = log({ console: true, file: false, label: config.name });
 
-app.use(express.urlencoded({ extended: true })); // express body-parser
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
-
-//Attempting to connect to the database.
-
-// connection.connect( function(err) {
-//   if(err)
-//   {
-//       throw err;
-//   }
-//   else
-//   {
-//     logger.info("connected to DB");
-//   }
-
-// });
-
 
 //connecting the express object to listen on a particular port as defined in the config object. 
 
@@ -66,10 +34,10 @@ app.listen(config.port, config.host, (e) => {
   logger.info(`${config.name} running on ${config.host}:${config.port}`);
 }); 
 
-// app.get('/', function(err, result)
-// {
-//   result.status(200).send("on 0.0.0.0:3000");
-// });
+app.get('/', function(err, result)
+{
+  result.status(200).send("on 0.0.0.0:3000");
+});
 
 var routes = require('./Router/routes.js');
 routes(app);
