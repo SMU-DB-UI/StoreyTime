1f71ca217a45032e012dd31b455d139aa9b08f8d
HaydenCenter <43649440+HaydenCenter@users.noreply.github.com>
2019-11-15 16:58:33 -0600

    Fixed routing to direct the default page to the home page (TODO: Handle preventing access to the homepage without a login token)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/home" component={Home}/>
-            <Route path="/" component={Login} />
+            <Route path="/" component={Home} />
           </Switch>
         </BrowserRouter>
       </div>
