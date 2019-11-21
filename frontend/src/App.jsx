c3afb7073fb0d83acb2d7f3e7fa08d0637d3a24d
HaydenCenter <43649440+HaydenCenter@users.noreply.github.com>
2019-11-21 17:27:58 -0600

    Started Polls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './components/login/Login';
 import Home from './components/home/Home'
+import Polls from './components/polls/Polls'
 import Register from './components/register/Register'
 import Profile from './components/profile/Profile'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
@@ -15,8 +16,9 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/home" component={Home}/>
+            <Route path="/polls" component={Polls}/>
             <Route path="/profile" component={Profile}/>
-            <Route path="/" component={Home} />
+            <Route path="/" component={Polls} />
           </Switch>
         </BrowserRouter>
       </div>
