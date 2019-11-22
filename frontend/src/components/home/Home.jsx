276e39a7a2ef6b7f47975eee38a7b4c4a283bab7
HaydenCenter <43649440+HaydenCenter@users.noreply.github.com>
2019-11-21 20:37:49 -0600

    Started new Home Page

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,35 +1,94 @@
 import React from 'react';
 import Navbar from '../navbar/Navbar'
-import HomeCard from '../homeCard/HomeCard'
+//import PollCard from '../pollCard/Polls'
 import './home.css';
 
-const Home = props => (
-    <>
-        <Navbar />
-        <div className='home-container'>
-            {/*Give each card an id so it can be populated separately*/}
-            <HomeCard title='Card Title'
-                name='Author Name'
-                time='11:59, 01/01/1970'
-                text='This is the body of the card. Information will be listed here such as details about a poll, candidate, or followed tag. They will be able to be sorted by their tags.'
-                tags={['Tag1', 'Tag2', 'Tag3']} />
-            <HomeCard title='Card Title'
-                name='Author Name'
-                time='11:59, 01/01/1970'
-                text='This is the body of the card. Information will be listed here such as details about a poll, candidate, or followed tag. They will be able to be sorted by their tags.'
-                tags={['Tag1', 'Tag2', 'Tag3']} />
-            <HomeCard title='Card Title'
-                name='Author Name'
-                time='11:59, 01/01/1970'
-                text='This is the body of the card. Information will be listed here such as details about a poll, candidate, or followed tag. They will be able to be sorted by their tags.'
-                tags={['Tag1', 'Tag2', 'Tag3']} />
-            <HomeCard title='Card Title'
-                name='Author Name'
-                time='11:59, 01/01/1970'
-                text='This is the body of the card. Information will be listed here such as details about a poll, candidate, or followed tag. They will be able to be sorted by their tags.'
-                tags={['Tag1', 'Tag2', 'Tag3']} />
-        </div>
-    </>
-);
+class Home extends React.Component {
+
+    state = {
+        polls: [
+            {
+                question: 'Democrat or Republican?',
+                answers: ['Democrat', 'Republican'],
+                votes: [51, 49],
+                id: 0,
+                tags: ['Parties', 'President', 'Alignment'],
+                user: 'Hayden Center',
+                date: '11/21/2019'
+            }
+        ],
+        posts: []
+    }
+    render() {
+        return (<>
+            <Navbar />
+            <div className='home-container'>
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col-lg-6 col-12">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col-6 text-left">
+                                            <h1 className="card-title">Card Title</h1>
+                                        </div>
+                                        <div className="col-6 text-right">
+                                            <button className="btn">
+                                                New Post
+                                                </button>
+                                        </div>
+                                    </div>
+                                    {this.state.posts.map(poll =>
+                                        <div className="row">
+                                            <div className="col-12">
+                                                {/* <PollCard poll={poll} /> */}
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">{poll.question}</h4>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>)}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6 col-12">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col-6 text-left">
+                                            <h1 className="card-title">Card Title</h1>
+                                        </div>
+                                        <div className="col-6 text-right">
+                                            <button className="btn">
+                                                New Poll
+                                                </button>
+                                        </div>
+                                    </div>
+                                    {this.state.polls.map(poll =>
+                                        <div className="row">
+                                            <div className="col-12">
+                                                {/* <PollCard poll={poll} /> */}
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">{poll.question}</h4>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>)}
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>);
+    }
+}
 
 export default Home;
