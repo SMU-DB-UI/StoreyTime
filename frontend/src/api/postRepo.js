af9b0f5a7b503c43e9e1e47cec04242e70e9a56d
Harrison Noble <harrisonnoble@yahoo.com>
2019-11-29 20:15:19 -0800

    post repository updated

--- frontend/src/api/postRepo.js
+++ frontend/src/api/postRepo.js
@@ -15,9 +15,25 @@ export class PostRepo {
         });
     }
 
+    addTags(post_id, tag_word) {
+        return new Promise((resolve, reject) => {
+            axios.put(this.url + 'user/newPost/addTags/' + localStorage.getItem('id'), {post_id, tag_word})
+                .then(resp => resolve(resp.data))
+                .catch(resp => reject(resp));
+        });
+    }
+
     editPost (post) {
         return new Promise((resolve, reject) => {
             axios.put(this.url + 'user/editPost/' + localStorage.getItem('id'), post)
+                .then(resp => resolve(resp.data))
+                .catch(resp => reject(resp));
+        });
+    }
+
+    deletePost(post_id){
+        return new Promise((resolve, reject) => {
+            axios.put(this.url + 'user/deletePost/' + localStorage.getItem('id'), post_id)
                 .then(resp => resolve(resp.data))
                 .catch(resp => reject(resp));
         });
